c64ebe8dcdbeeacf9cd0e7127fe958d0235a13b2
deideideideidei <83042548+deideideideidei@users.noreply.github.com>
2021-04-22 18:48:43 -0400

    archivos
    se agregan uwu

---
+++ login_module_dev_repo-develop_D/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace login_module_dev_repo_develop_D.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
